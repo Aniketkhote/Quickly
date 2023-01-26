56c6963dbc94c7dd39adf6352232bfe23407e2af
Aniket Khote <aniketkhote99@gmail.com>
2023-01-27 00:53:31 +0530

    removed 'prefer_foreac' from analysis_options.yaml

--- lib/src/dart/int_extension.dart
+++ lib/src/dart/int_extension.dart
@@ -1,45 +1,55 @@
-///Int extension to extend List functionality
+/// IntExtension is an extension method on the int class that provides additional functionality for working with months and weeks.
 extension IntExtension on int {
-  /// Get Month name from number
+  /// Extension method to get the name of a month from a given number.
   ///
-  /// Example:
+  /// Example usage:
   /// ```dart
-  /// 1.getMonthName()         // January
-  /// 2.getMonthName(true)    // Feb
+  /// 1.getMonthName() => "January"
+  /// 2.getMonthName(true) => "Feb"
   /// ```
-  String getMonthName([bool isShort = false]) =>
-      <int, String>{
-        1: isShort ? 'Jan' : 'January',
-        2: isShort ? 'Feb' : 'February',
-        3: isShort ? 'Mar' : 'March',
-        4: isShort ? 'Apr' : 'April',
-        5: isShort ? 'May' : 'May',
-        6: isShort ? 'Jun' : 'June',
-        7: isShort ? 'Jul' : 'July',
-        8: isShort ? 'Aug' : 'August',
-        9: isShort ? 'Sept' : 'September',
-        10: isShort ? 'Oct' : 'October',
-        11: isShort ? 'Nov' : 'November',
-        12: isShort ? 'Dec' : 'December',
-      }[this] ??
-      'Invalid number of month';
+  ///
+  /// @param [isShort] If true, returns the short form of the month name. Default is false.
+  /// @return Returns the name of the month as a string. If the input number is not a valid month, returns "Invalid number of month".
+  String getMonthName([bool isShort = false]) {
+    Map<int, String> monthNames = <int, String>{
+      1: isShort ? 'Jan' : 'January',
+      2: isShort ? 'Feb' : 'February',
+      3: isShort ? 'Mar' : 'March',
+      4: isShort ? 'Apr' : 'April',
+      5: isShort ? 'May' : 'May',
+      6: isShort ? 'Jun' : 'June',
+      7: isShort ? 'Jul' : 'July',
+      8: isShort ? 'Aug' : 'August',
+      9: isShort ? 'Sept' : 'September',
+      10: isShort ? 'Oct' : 'October',
+      11: isShort ? 'Nov' : 'November',
+      12: isShort ? 'Dec' : 'December',
+    };
 
-  /// Get Week name from number
+    return monthNames[this] ?? 'Invalid number of month';
+  }
+
+  /// Extension method to get the name of a week from a given number.
   ///
-  /// Example:
+  /// Example usage:
   /// ```dart
-  /// 1.getWeekName()         // Monday
-  /// 2.getWeekName(true)    // Tue
+  /// 1.getWeekName() => "Monday"
+  /// 2.getWeekName(true) => "Tue"
   /// ```
-  String getWeekName([bool isShort = false]) =>
-      <int, String>{
-        1: isShort ? 'Mon' : 'Monday',
-        2: isShort ? 'Tue' : 'Tuesday',
-        3: isShort ? 'Wed' : 'Wednesday',
-        4: isShort ? 'Thu' : 'Thursday',
-        5: isShort ? 'Fri' : 'Friday',
-        6: isShort ? 'Sat' : 'Saturday',
-        7: isShort ? 'Sun' : 'Sunday',
-      }[this] ??
-      'Invalid number of month';
+  ///
+  /// @param [isShort] If true, returns the short form of the week name. Default is false.
+  /// @return Returns the name of the week as a string. If the input number is not a valid week, returns "Invalid number of week".
+  String getWeekName([bool isShort = false]) {
+    Map<int, String> weekNames = <int, String>{
+      1: isShort ? 'Mon' : 'Monday',
+      2: isShort ? 'Tue' : 'Tuesday',
+      3: isShort ? 'Wed' : 'Wednesday',
+      4: isShort ? 'Thu' : 'Thursday',
+      5: isShort ? 'Fri' : 'Friday',
+      6: isShort ? 'Sat' : 'Saturday',
+      7: isShort ? 'Sun' : 'Sunday',
+    };
+
+    return weekNames[this] ?? 'Invalid number of week';
+  }
 }
--- lib/src/dart/num_extension.dart
+++ lib/src/dart/num_extension.dart
@@ -1,6 +1,16 @@
-///Num extension to extend List functionality
+/// Extends the functionality of the `num` class by adding various methods for formatting and working with numbers.
 extension NumExtension on num {
-  /// Add suffix to the number
+  /// Adds a suffix to the number.
+  ///
+  /// This method takes an optional parameter [suffix],
+  /// which is a string that will be appended to the number.
+  /// If [suffix] is not provided, it defaults to an ordinal suffix (e.g. 1st, 2nd, 3rd, etc.).
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   print(5.suffix()); // prints "5th"
+  ///   print(5.suffix('%')); // prints "5%"
+  /// ```
   String suffix([String suffix = '']) {
     if (suffix.isNotEmpty) return '${this}$suffix';
     return <int, String>{
@@ -11,21 +21,57 @@ extension NumExtension on num {
         '${this}th';
   }
 
-  ///Returns Duration of this in seconds.
+  /// Returns a `Duration` object representing the number of seconds represented by this number.
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   var fiveSeconds = 5.seconds;
+  ///   print(fiveSeconds); // prints "Duration: 00:00:05.000000"
+  /// ```
   Duration get seconds => Duration(seconds: this as int);
 
-  ///Returns Duration of this in milliseconds.
+  /// Returns a `Duration` object representing the number of milliseconds represented by this number.
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   var fiveMilliseconds = 5.milliseconds;
+  ///   print(fiveMilliseconds); // prints "Duration: 0:00:00.005000"
+  /// ```
   Duration get milliseconds => Duration(milliseconds: this as int);
 
-  ///Returns Duration of this in microseconds.
+  /// Returns a `Duration` object representing the number of microseconds represented by this number.
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   var fiveMicroseconds = 5.microseconds;
+  ///   print(fiveMicroseconds); // prints "Duration: 0:00:00.000005"
+  /// ```
   Duration get microseconds => Duration(microseconds: this as int);
 
-  ///Returns Duration of this in minutes.
+  /// Returns a `Duration` object representing the number of minutes represented by this number.
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   var fiveMinutes = 5.minutes;
+  ///   print(fiveMinutes); // prints "Duration: 0:05:00.000000"
+  /// ```
   Duration get minutes => Duration(minutes: this as int);
 
-  ///Returns Duration of this in hours.
+  /// Returns a `Duration` object representing the number of hours represented by this number.
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   var fiveHours = 5.hours;
+  ///   print(fiveHours); // prints "Duration: 5:00:00.000000"
+  /// ```
   Duration get hours => Duration(hours: this as int);
 
-  ///Returns Duration of this in days.
+  /// Returns a `Duration` object representing the number of days represented by this number.
+  ///
+  /// Example usage:
+  /// ```dart
+  ///   var fiveDays = 5.days;
+  ///   print(fiveDays); // prints "Duration: 120:00:00.000000"
+  /// ```
   Duration get days => Duration(days: this as int);
 }
