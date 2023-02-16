8183b3700dd03afc52d76c2e3eefefdb2bf693ec
Aniket Khote <aniketkhote99@gmail.com>
2023-02-16 12:43:21 +0530

    navigation merge to main branch

--- lib/quickly.dart
+++ lib/quickly.dart
@@ -7,3 +7,5 @@ export 'src/flutter/mixins/mixins.dart';
 export 'src/flutter/extensions/extensions.dart';
 
 export 'src/flutter/responsive.dart';
+
+export 'src/flutter/navigation/fx_navigation.dart';
