44be062b3630efc1f2ce60a6c8b9fdf6d3c8cd8e
Aniket Khote <aniketkhote99@gmail.com>
2023-05-28 21:51:07 +0530

    comment added to FxButton

--- lib/src/flutter/widget/button.dart
+++ lib/src/flutter/widget/button.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../../../quickly.dart';
 
+/// A customizable button widget that follows a specific design style.
+///
+/// The [FxButton] widget provides various customization options such as text,
+/// icons, colors, shapes, sizes, and more. It supports different button types
+/// like solid, outline, outline2x, and transparent. The appearance and behavior
+/// of the button can be modified using the provided properties.
 class FxButton extends StatelessWidget {
   const FxButton({
     required this.text,
@@ -23,36 +29,65 @@ class FxButton extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  /// The [text] parameter is required and specifies the button's label text.
   final String text;
 
+  /// The [onPressed] parameter is required and defines the callback function
+  /// when the button is pressed.
+  final VoidCallback onPressed;
+
+  /// The [leadingIcon] and [trailingIcon] parameters specify the icons to be
+  /// displayed before and after the button text, respectively.
   final IconData? leadingIcon;
 
   final IconData? trailingIcon;
 
-  final VoidCallback onPressed;
-
+  /// The [padding]  parameters allow customization of the button's internal padding
   final EdgeInsetsGeometry? padding;
 
+  /// The [margin] parameters allow customization of the button's external padding.
   final EdgeInsetsGeometry? margin;
 
+  /// The [radius] parameter determines the button's border radius. If not
+  /// provided, the default border radius will be based on the button's shape.
   final BorderRadius? radius;
 
+  /// The [outlineColor] parameter sets the button's outline color for the
+  /// outline button types.
   final Color outlineColor;
 
+  /// The [color] parameter sets the button's background color for the solid
+  /// button type.
   final Color? color;
 
+  /// The [textColor] parameter sets the text color for the solid button type.
+  /// If not provided, the text color will be automatically determined based on
+  /// the button's background color.
   final Color? textColor;
 
+  /// The [iconColor] parameter sets the color for the leading and trailing
+  /// icons. If not provided, the icon color will be determined based on the
+  /// button's background color.
   final Color? iconColor;
 
+  /// The [shadow] parameter allows specifying a list of box shadows to create
+  /// a shadow effect for the button.
   final List<BoxShadow>? shadow;
 
+  /// The [shape] parameter specifies the button's shape, which can be one of
+  /// the values in the [BtnShape] enum.
   final BtnShape shape;
 
+  /// The [size] parameter determines the button's size, which can be one of
+  /// the values in the [BtnSize] enum.
   final BtnSize size;
 
+  /// The [type] parameter defines the button's type, which can be one of the
+  /// values in the [BtnType] enum.
   final BtnType type;
 
+  /// The [isBlock] parameter indicates whether the button should occupy the
+  /// full width available.
   final bool isBlock;
 
   @override
@@ -163,8 +198,30 @@ class FxButton extends StatelessWidget {
   }
 }
 
+/// Defines the possible shapes for the [FxButton].
+///
+/// The [pill] shape creates a pill-shaped button with rounded edges.
+/// The [flat] shape represents a flat button with no specific shape.
+/// The [rounded] shape creates a button with rounded corners.
+/// The [leaf] shape creates a button with one rounded corner on the top-right
+/// and one rounded corner on the bottom-left, giving it a leaf-like appearance.
+/// The [rleft] shape creates a button with a rounded corner on the left side.
+/// The [rright] shape creates a button with a rounded corner on the right side.
 enum BtnShape { pill, flat, rounded, leaf, rleft, rright }
 
+/// Defines the possible types for the [FxButton].
+///
+/// The [solid] type represents a button with a solid background color.
+/// The [outline] type represents a button with an outline border and no fill.
+/// The [outline2x] type represents a button with a thicker outline border and no fill.
+/// The [transparent] type represents a button with a transparent background.
 enum BtnType { solid, outline, outline2x, transparent }
 
+/// Defines the possible sizes for the [FxButton].
+///
+/// The [tiny] size represents the smallest size for the button.
+/// The [small] size represents a small-sized button.
+/// The [normal] size represents a medium-sized button.
+/// The [medium] size represents a large-sized button.
+/// The [large] size represents the largest size for the button.
 enum BtnSize { tiny, small, normal, medium, large }
